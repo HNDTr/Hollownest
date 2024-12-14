caed07538c7f81d4dfa3db731ebcfe3404d7c155
Huy Tran <huytrn13@gmail.com>
2024-12-14 11:13:21 -0600

    added toggle section feature

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.querySelectorAll('.region').forEach(region => {
         const regionName = this.getAttribute('data-name');
         const regionInfo = document.querySelector('#region-info');
         const regionNameElement = document.querySelector('#region-name');
+       
         
         regionNameElement.textContent = regionName;
         regionInfo.style.display = 'block';
@@ -12,4 +13,23 @@ document.querySelectorAll('.region').forEach(region => {
         const regionInfo = document.querySelector('#region-info');
         regionInfo.style.display = 'none';
     });
+
+    region.addEventListener('click', function(){
+        const regionSectionInfo = document.querySelector(`#${region.id}-Info`);
+        hideSections(regionSectionInfo);
+        if (regionSectionInfo) {
+            // Check if the section is hidden or not, including unset styles
+            const isHidden = !regionSectionInfo.style.display || regionSectionInfo.style.display === 'none';
+            regionSectionInfo.style.display = isHidden ? 'block' : 'none';
+        }
+    }) 
 });
+
+function hideSections(currentSection){
+    document.querySelectorAll('.info-section').forEach(section => {
+        if (section.id !== currentSection.id){
+            section.style.display = 'none';
+        }
+    })
+}
+
